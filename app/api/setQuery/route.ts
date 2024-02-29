f29da1089c71d16302a8c224decfda828fb7aced
usounds <grave.evarg@gmail.com>
2024-02-29 20:51:28 +0900

    認証のバグの修正

--- app/api/setQuery/route.ts
+++ app/api/setQuery/route.ts
@@ -1,7 +1,7 @@
 export const runtime = 'edge';
 export async function POST(request: Request) {
     const body = await request.json()
-    if(process.env.PRODUCTION_MODE==='1' && request.headers.get('origin')!=='https://starrysky-console.pages.dev/'){
+    if(process.env.PRODUCTION_MODE==='1' && request.headers.get('origin')!=='https://starrysky-console.pages.dev'){
         return new Response('error', {
             status: 500
         })
