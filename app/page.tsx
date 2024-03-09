6a9f1904c368650e433aade5b60357ff0e62ace8
usounds <grave.evarg@gmail.com>
2024-03-10 04:32:34 +0900

    レイアウト調整

--- app/page.tsx
+++ app/page.tsx
@@ -911,12 +911,12 @@ export default function Home() {
                 <div className="-m-1.5 overflow-x-auto">
                   <div className="p-1.5 min-w-full inline-block align-middle">
                     <div className="overflow-hidden">
-                      <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700 table-auto">
+                      <table className="table-fixed min-w-full divide-y divide-gray-200 dark:divide-gray-700 table-auto">
                         <thead>
                           <tr>
-                            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
-                            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Time</th>
-                            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Text</th>
+                            <th scope="col" className="w-1/8 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                            <th scope="col" className="w-1/8 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Time</th>
+                            <th scope="col" className="w-6/8 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Text</th>
                           </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
