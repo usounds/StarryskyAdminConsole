0e057586f77a0bfc5c40d01ac415a375fb24c3c0
usounds <grave.evarg@gmail.com>
2024-03-12 02:14:11 +0900

    エラーハンドリングを修正

--- app/api/getD1Query/route.ts
+++ app/api/getD1Query/route.ts
@@ -8,10 +8,10 @@ export async function POST(request: Request) {
 	const { env } = getRequestContext()
 	const myDb = env.DB
   let results 
-
   let resultObject
 
-  if(serverUrl!== undefined || serverUrl ==='' || serverUrl === null){
+  /*
+  if(serverUrl === undefined || serverUrl ==='' || serverUrl === null){
     resultObject = {
       result : 'SERVER_URL_IS_REQUIRED'
 
@@ -25,6 +25,7 @@ export async function POST(request: Request) {
     })
 
   }
+  */
 
   if(key !== undefined && key!=='' && key!== null) results = await myDb.prepare('select * from conditions where serverUrl = ? and key = ?').bind(serverUrl,key).all()
   else results = await myDb.prepare('select * from conditions where serverUrl = ? ').bind(serverUrl).all()
